774950ed7a720bad2670bdf131526f27b4e26b45
fabien-gigante <fabien.gigante@free.fr>
2024-02-15 12:42:40 +0100

    code clean up

--- src/main/java/com/fabien_gigante/PipeBlock.java
+++ src/main/java/com/fabien_gigante/PipeBlock.java
@@ -16,7 +16,6 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -29,56 +28,52 @@ public class PipeBlock
 extends PillarBlock
 implements Waterloggable {
     public static final MapCodec<PipeBlock> CODEC = PipeBlock.createCodec(PipeBlock::new);
-    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
     protected static final VoxelShape Y_SHAPE = Block.createCuboidShape(6, 0, 6, 10, 16, 10);
     protected static final VoxelShape Z_SHAPE = Block.createCuboidShape(6, 4, 0, 10, 8, 16);
     protected static final VoxelShape X_SHAPE = Block.createCuboidShape(0, 4, 6, 16, 8, 10);
 
-    public static final PipeBlock PIPE = new PipeBlock(FabricBlockSettings.create().mapColor(MapColor.ORANGE).solid().requiresTool().strength(4.0f, 5.0f).sounds(BlockSoundGroup.COPPER).nonOpaque());
+    public static final PipeBlock PIPE = new PipeBlock(
+        FabricBlockSettings.create().mapColor(MapColor.ORANGE).solid().requiresTool()
+        .strength(4.0f, 5.0f).sounds(BlockSoundGroup.COPPER).nonOpaque());
 
     public MapCodec<PipeBlock> getCodec() { return CODEC; }
 
     public PipeBlock(AbstractBlock.Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(WATERLOGGED, false).with(AXIS, Direction.Axis.Y));
+        this.setDefaultState(this.getDefaultState().with(Properties.WATERLOGGED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(Properties.WATERLOGGED);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        switch (state.get(AXIS)) {
-            case X: return X_SHAPE;
-            case Z: return Z_SHAPE;
-            default: return Y_SHAPE;
-        }
+        return switch (state.get(AXIS)) {case X -> X_SHAPE; case Z-> Z_SHAPE; default -> Y_SHAPE; };
     }
 
     @Override
     @Nullable
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
-        boolean bl = fluidState.getFluid() == Fluids.WATER;
-        return (BlockState)super.getPlacementState(ctx).with(WATERLOGGED, bl);
+        boolean isWater = fluidState.getFluid() == Fluids.WATER;
+        return super.getPlacementState(ctx).with(Properties.WATERLOGGED, isWater);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        if (state.get(WATERLOGGED).booleanValue()) {
-            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-        }
+        if (state.get(Properties.WATERLOGGED)) world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(WATERLOGGED).add(AXIS);
-    }
-
+    @SuppressWarnings("deprecation")
     @Override
     public FluidState getFluidState(BlockState state) {
-        if (state.get(WATERLOGGED).booleanValue()) {
-            return Fluids.WATER.getStill(false);
-        }
-        return super.getFluidState(state);
+        return state.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
     }
 
     @Override
