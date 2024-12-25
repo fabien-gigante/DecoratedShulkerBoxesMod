42f4352a6ef2d8e969856f2c4972f3092e4a0593
fabien-gigante <fabien.gigante@free.fr>
2024-12-25 10:49:37 +0100

    tidy

--- src/main/java/com/fabien_gigante/mixin/TransmuteRecipeMixin.java
+++ src/main/java/com/fabien_gigante/mixin/TransmuteRecipeMixin.java
@@ -2,19 +2,17 @@ package com.fabien_gigante.mixin;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import com.fabien_gigante.DecoratedShulkerBoxItemStack;
 import com.fabien_gigante.IDecoratedShulkerBox;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.item.DyeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -23,6 +21,7 @@ import net.minecraft.recipe.TransmuteRecipe;
 import net.minecraft.recipe.input.CraftingRecipeInput;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.DyeColor;
 import net.minecraft.world.World;
 
@@ -30,35 +29,26 @@ import net.minecraft.world.World;
 public class TransmuteRecipeMixin {
 	@Shadow Ingredient input, material;
 	@Shadow RegistryEntry<Item> result;
-	@Shadow String group;
 
 	// Helpers to search the recipe inventory
-	@Unique
 	private static Stream<ItemStack> search(CraftingRecipeInput input, Predicate<ItemStack> condition) {
 		return input.getStacks().stream().filter(condition);
 	}
-	@Unique
+	private static <T> T onlyIfElse(T a, boolean b, T c) { return b ? a : c; }
 	private static ItemStack single(CraftingRecipeInput input, Predicate<ItemStack> condition) {
 		Iterator<ItemStack> it = search(input,condition).iterator();
-		if (!it.hasNext()) return ItemStack.EMPTY;
-		ItemStack first = it.next();
-		return it.hasNext() ? ItemStack.EMPTY : first;
-	}
-	@Unique
-	private static boolean isShulkerBox(ItemStack stack) {
-		return Block.getBlockFromItem(stack.getItem()) instanceof ShulkerBoxBlock; 
+		return it.hasNext() ? onlyIfElse(it.next(), !it.hasNext(), ItemStack.EMPTY) : ItemStack.EMPTY ;
 	}
 
 	/** @reason using overwrite because behavior change not easy by simple code injection @author fabien **/
 	@Overwrite
 	public boolean matches(CraftingRecipeInput input, World world) {
 		// Matches standard recipe
-		ItemStack inputItem = single(input, (stack) -> this.input.test(stack));
-		if (inputItem == ItemStack.EMPTY) return false;
+		if (single(input, (stack) -> this.input.test(stack)) == ItemStack.EMPTY) return false;
 		if (single(input, (stack) -> this.material.test(stack)) == ItemStack.EMPTY) return false;
 		if (input.getStackCount() == 2) return true;
 		// Matches extra dye recipe
-		if (!isShulkerBox(inputItem) || input.getStackCount() != 3) return false;
+		if (input.getStackCount() != 3 || !this.result.isIn(ItemTags.SHULKER_BOXES)) return false;
 		List<ItemStack> dyes = search(input, (stack) -> stack.getItem() instanceof DyeItem).toList();
 		return dyes.size() == 2 && this.material.test(dyes.get(0)) && !this.material.test(dyes.get(1));
 	}
@@ -70,7 +60,7 @@ public class TransmuteRecipeMixin {
 		ItemStack inputItem = single(input, (stack) -> this.input.test(stack));
 		ItemStack craftedItem = inputItem.copyComponentsToNewStack(this.result.value(), 1);
 		// Additional behavior for extra dye recipe
-		if (isShulkerBox(craftedItem)) {
+		if (this.result.isIn(ItemTags.SHULKER_BOXES)) {
 			ItemStack extraDye = single(input, (stack) -> stack.getItem() instanceof DyeItem && !this.material.test(stack));
 			DyeColor secondaryColor = extraDye.getItem() instanceof DyeItem dye ? dye.getColor() : null;
 			IDecoratedShulkerBox decorated = DecoratedShulkerBoxItemStack.from(wrapperLookup, craftedItem);
