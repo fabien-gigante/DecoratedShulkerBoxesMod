299a5597bebbbfa1cf0c7734721ba5f2627147ce
fabien-gigante <fabien.gigante@free.fr>
2025-12-28 00:19:54 +0100

    fix rename

--- src/client/java/com/fabien_gigante/mixin/AnvilScreenMixin.java
+++ src/client/java/com/fabien_gigante/mixin/AnvilScreenMixin.java
@@ -30,7 +30,7 @@ public abstract class AnvilScreenMixin extends AbstractContainerScreen<AnvilMenu
             this.shulkerBoxName = itemStack.is(ItemTags.SHULKER_BOXES) && !itemStack.has(DataComponents.CUSTOM_NAME) ? itemStack.getHoverName().getString() : "";
         if (i == 1)
             this.displayedItemName = itemStack.isEmpty() ? "" : itemStack.getHoverName().getString();
-        if (!this.shulkerBoxName.isEmpty()) {
+        if ((i == 0 || i == 1) && !this.shulkerBoxName.isEmpty()) {
 			this.name.setValue(this.displayedItemName.isEmpty() ? this.shulkerBoxName : Component.translatable("decorated.shulkerbox.name", this.displayedItemName).getString());
 			this.name.setEditable(true);
 			this.setFocused(this.name);
